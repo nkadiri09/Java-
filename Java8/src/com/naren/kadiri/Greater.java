f335dbfbf453143a024e04ba0f83afd89d476fca
nkadiri09 <nkadiri09@gmail.com>
2019-03-20 23:13:06 -0500

    initial commit

---
+++ Java8/src/com/naren/kadiri/Greater.java
@@ -0,0 +1,14 @@
+package com.naren.kadiri;
+
+public class Greater {
+
+	public static void main(String[] args) {
+		Greater greate = new Greater();
+		greate.greet();
+	}
+
+	public void greet() {
+		System.out.println("Hello World !.");
+	}
+
+}
